403ed15c1fc580dff3e0fe1d93ff0d9527646851
Feelcover <pioneerbeat@yandex.ru>
2024-01-09 14:01:05 +0300

    сделать простую начальную структуру

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+import styles from './Layout.module.scss'
+
+const Layout = () => {
+  return (
+    <div className={styles.layout}>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Layout

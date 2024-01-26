a34bdaab8c701ffadca2d7a02bb28e951ceac55c
Feelcover <pioneerbeat@yandex.ru>
2024-01-26 15:37:04 +0300

    структура header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,14 @@ import styles from './Header.module.scss'
 const Header = () => {
   return (
     <header className={styles.header}>
-      Header
+      <div className={styles.logoContainer}>Logo</div>
+      <div className={styles.menuContainer}>
+        <nav className={styles.menu}>
+          <div className={styles.menu__item}>Поиск</div>
+          <div className={styles.menu__item}></div>
+          <div className={styles.menu__item}>Личный кабинет</div>
+        </nav>
+      </div>
     </header>
   )
 }

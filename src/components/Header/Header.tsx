c254746b0d8ef6a58702f96b734b509c7a6f5762
Feelcover <pioneerbeat@yandex.ru>
2024-01-27 13:59:13 +0300

    сверстать часть хедера

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,14 @@ import styles from './Header.module.scss'
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>Logo</div>
+      <div className={styles.logoContainer}>
+        <div className={styles.logo}/>
+      </div>
       <div className={styles.menuContainer}>
+      <input className={styles.search} type="text" placeholder='Поиск...'/>
         <nav className={styles.menu}>
-          <div className={styles.menu__item}>Поиск</div>
-          <div className={styles.menu__item}></div>
-          <div className={styles.menu__item}>Личный кабинет</div>
+          <div className={styles.menu__item}>Вход</div>
+          <div className={styles.menu__item}>Регистрация</div>
         </nav>
       </div>
     </header>

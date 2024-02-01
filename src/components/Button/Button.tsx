ccf4f53112561da215f5a0f5c583b026cb8e63e9
Feelcover <pioneerbeat@yandex.ru>
2024-02-01 20:28:27 +0300

    сверстать компонент button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import styles from "./Button.module.scss";
+
+interface IButton {
+  type: "button" | "submit" | "reset";
+  text: string;
+  width: number;
+  height: number;
+  backgroundColor?:string,
+  color?:string,
+  theme?: string;
+}
+
+const Button: FC<IButton> = ({
+  type = "button",
+  text,
+  width,
+  height,
+  backgroundColor,
+  color,
+  theme,
+}) => {
+  return (
+    <button
+      className={styles.button}
+      type={type}
+      style={{ width: width, height: height, backgroundColor: backgroundColor, color:color }}
+    >
+      <span className={styles.button__text}>{text}</span>
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,26 @@
-import { Link } from 'react-router-dom'
-import styles from './Header.module.scss'
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+import Button from "../Button/Button";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
-        <div className={styles.logo}/>
+        <div className={styles.logo} />
       </div>
       <div className={styles.menuContainer}>
-      <input className={styles.search} type="text" placeholder='Поиск...'/>
+        <input className={styles.search} type="text" placeholder="Поиск..." />
         <nav className={styles.menu}>
-          <Link className={styles.menu__item} to='/'>Вход</Link>
-          <Link className={styles.menu__item} to='/'>Регистрация</Link>
+          <Link className={styles.menu__item} to="/">
+            <Button type="button" height={32} width={72} backgroundColor="#f2f2f2" color="#000000" text="Вход" />
+          </Link>
+          <Link className={styles.menu__item} to="/">
+            <Button type="button" height={32} width={125} text="Регистрация" />
+          </Link>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
